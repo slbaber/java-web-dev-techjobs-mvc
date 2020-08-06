65a8804a635a6548bab4f77261795b2a88a5b4fa
slbaber <slbaber2@gmail.com>
2020-08-06 17:55:53 -0400

    Assignment 3

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,22 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping
+    public String searchDisplayResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+
+        model.addAttribute("columns", columnChoices);
+
+        if (searchType.toLowerCase().equals("all") || searchTerm.toLowerCase().equals("")) {
+            jobs = JobData.findAll();
+
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
+
 }
